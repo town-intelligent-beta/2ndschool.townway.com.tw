dff355b2c95128829ae26836d449c99d358f736e
HuangJyunYu <yillkid@gmail.com>
2025-07-19 14:54:34 +0800

    Update config.js

--- static/js/config.js
+++ static/js/config.js
@@ -6,15 +6,15 @@ HOST_URL_TPLANET_DAEMON = "https://rc-tplanet-backend.4impact.cc";
 HOST_URL_LLMTWINS = "https://phidata.4impact.cc"
 
 // EID
-HOST_URL_EID = "https://eid.townway.com.tw";
+HOST_URL_EID = "https://eid-stu.townway.com.tw";
 
 // Site hoster
-SITE_HOSTERS = ["forus999@gmail.com", "secondhome2023.1@gmail.com", "mickeypeng@tpwl.org"]
+SITE_HOSTERS = ["Real.ability2022@gmail.com"]
 
 // Site information
 SITE_TYPE = 0 // 0 or 1(公板)
-SITE_NAME = "Second Home"
-COPYRIGHT = "Second Home"
+SITE_NAME = "Second School"
+COPYRIGHT = "Second School"
 
 // Weight : SDGs, 人文地產景, 德智體群美
 WEIGHT = [1, 1, 1]
